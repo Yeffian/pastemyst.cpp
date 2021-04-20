a3e34992c1ad168be15907b9a71e9f2d949aacec
Yeff <76024824+YeffyCodeGit@users.noreply.github.com>
2021-04-20 18:44:17 +0600

    Added the premake and bat scripts

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+workspace "pastemyst.cpp"
+    configurations
+    {
+        "Debug",   -- Debugging
+        "Release", -- Internal testing
+        "Dist"     -- Final distrubutions
+    }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.architecture}"
+
+  project "pastemyst"
+      location "pastemyst.cpp"
+      kind "StaticLib"
+      language "C++"
+
+      cppdialect "C++11"
+      staticruntime "on"
+      systemversion "latest"
+
+      targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+      objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+      files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.hpp", "%{prj.name}/src/**.cpp" }
+      includedirs { "%{prj.name}/include"  }
